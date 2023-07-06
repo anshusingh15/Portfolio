0d49fcca45649a00072cac6829a3e754d6483cd7
ANSHU SINGH <121624962+anshusingh15@users.noreply.github.com>
2023-07-06 10:01:49 +0530

    Add files via upload

---
+++ js/sendMail.js
@@ -0,0 +1,11 @@
+/*===== SEND MAIL =====*/
+// function sendMail(){
+//     var params={
+//         from_name : document.getElementById("fullName").value,
+//         email_id : document.getElementById("email_id").value,
+//         message : document.getElementById("message").value
+//     }
+//     emailjs.send("service_f6nubcf","template_9i6nt2p",params).then(function(res){
+//         alert("success!!"+res.status);
+//     })
+// }
